290a1e3209968332cbfc3d92dade2f3a53a9d1bf
DELL <siralex2411@gmail.com>
2022-07-14 01:55:11 +0700

    upload source

--- script.js
+++ script.js
@@ -19,4 +19,9 @@ window.onscroll = () =>{
     document.querySelector('#scroll-top').classList.remove('active');
   }
 
+  if(window.scrollY > 60){
+    document.querySelector('select').classList.add('fixed');
+  }else{
+    document.querySelector('select').classList.remove('fixed');
+  }
 }
